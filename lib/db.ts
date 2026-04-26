ff798a2d4157eda696c545c1ed9a063a788c90e4
Gyane-git <alpen850@gmail.com>
2026-04-26 16:17:20 +0545

    updqte

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,25 @@
+// import mysql from "mysql2/promise";
+
+// let pool: mysql.Pool | null = null;
+
+// export function getPool() {
+//   if (!pool) {
+//     pool = mysql.createPool({
+//       host: "localhost",
+//       user: "root",
+//       password: "",
+//       database: "studysync",
+//       waitForConnections: true,
+//       connectionLimit: 10,
+//       queueLimit: 0,
+//     });
+//   }
+//   return pool;
+// }
+
+// export default getPool;
+
+
 import mysql from "mysql2/promise";
 
 let pool: mysql.Pool | null = null;
@@ -5,10 +27,10 @@ let pool: mysql.Pool | null = null;
 export function getPool() {
   if (!pool) {
     pool = mysql.createPool({
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "studysync",
+      host: process.env.DB_HOST || "localhost",
+      user: process.env.DB_USER || "",
+      password: process.env.DB_PASSWORD || "",
+      database: process.env.DB_NAME || "",
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0,
